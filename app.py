a6933f30fbe3ed6c6323266d3ef04a644cecbb7a
Biswajit Mohapatra <biswajitmohapatrachem170@gmail.com>
2025-09-22 13:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from model import load_documents, create_or_load_index, chat_with_agent
 import time
 
-st.set_page_config(page_title="BiswaLex", page_icon="⚛", layout="wide")
+st.set_page_config(page_title="BiswaLex", page_icon="⚛️", layout="wide")
 
 # --- Initialize index and sessions ---
 if 'index' not in st.session_state:
@@ -105,7 +105,7 @@ for msg in st.session_state.current_session:
     content = msg['message']
     if msg['role'] == "Agent":
         st.markdown(
-            f"<div style='text-align:left; margin:5px 0;'>⚛ <b>{content}</b></div>",
+            f"<div style='text-align:left; margin:5px 0;'>⚛️ <b>{content}</b></div>",
             unsafe_allow_html=True
         )
     else:  # User
@@ -118,3 +118,4 @@ for msg in st.session_state.current_session:
 if st.sidebar.button("Save Session"):
     if st.session_state.current_session not in st.session_state.sessions:
         st.session_state.sessions.append(st.session_state.current_session.copy())
+
